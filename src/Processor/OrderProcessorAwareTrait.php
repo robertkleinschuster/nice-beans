d8c5c63cb7aaec3a39d47217c4f522f29298c672
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-11-30 18:46:10 +0100

    OrderProcessorAwareTrait.php

---
+++ src/Processor/OrderProcessorAwareTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Niceshops\Bean\Processor;
+
+
+class OrderProcessorAwareTrait
+{
+    private ?BeanOrderProcessor $beanOrderProcessor = null;
+
+    /**
+     * @return BeanOrderProcessor
+     */
+    public function getBeanOrderProcessor(): BeanOrderProcessor
+    {
+        return $this->beanOrderProcessor;
+    }
+
+    /**
+     * @param BeanOrderProcessor $beanOrderProcessor
+     *
+     * @return $this
+     */
+    public function setBeanOrderProcessor(BeanOrderProcessor $beanOrderProcessor): self
+    {
+        $this->beanOrderProcessor = $beanOrderProcessor;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasBeanOrderProcessor(): bool
+    {
+        return isset($this->beanOrderProcessor);
+    }
+
+
+}
